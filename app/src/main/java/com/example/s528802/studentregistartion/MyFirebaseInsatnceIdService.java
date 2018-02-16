fd5af2bc9da52c2ac507c52e3be0ca29251fee6c
Swaroopg79 <s528802@mail.nwmissouri.edu>
2018-02-16 09:25:08 -0600

    After inserting data into firebase with image and authentication setup

--- app/src/main/java/com/example/s528802/studentregistartion/MyFirebaseInsatnceIdService.java
+++ app/src/main/java/com/example/s528802/studentregistartion/MyFirebaseInsatnceIdService.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by S528802 on 2/8/2018.
  */
@@ -14,8 +16,8 @@ public class MyFirebaseInsatnceIdService extends FirebaseInstanceIdService {
     private static final String REG_TOKEN = "REG_TOKEN";
     @Override
     public void onTokenRefresh() {
-        String recent_token = FirebaseInstanceId.getInstance().getToken();
-        Log.d(REG_TOKEN,recent_token);
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG,"REG_TOKEN"+token);
 
 
     }
