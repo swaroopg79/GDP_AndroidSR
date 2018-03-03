c78de7f1db888f657118ff6223221b60198ce58f
Swaroopg79 <s528802@mail.nwmissouri.edu>
2018-03-02 21:16:46 -0600

    Retriving values from profile page

--- app/src/main/java/com/example/s528802/studentregistartion/ProfileActivity.java
+++ app/src/main/java/com/example/s528802/studentregistartion/ProfileActivity.java
@@ -1,29 +1,87 @@
 package com.example.s528802.studentregistartion;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
-    //view objects
     private TextView textViewUserEmail;
+
+
+    //view objects
+    private EditText editDisasterName;
+    private EditText editCriticality;
+    ImageView disasterImage;
+    private Button buutonUploadImage;
+    private Button chooseButtonDisaster;
     private Button buttonLogout;
 
+
+    // Folder path for Firebase Storage.
+    String Storage_Path = "Disaster_Image_Uploads/";
+
+    // Root Database Name for Firebase Database.
+    String Database_Path = "Student_Database/";
 // Have to include all the functionalities for uploading photo from camera
+// Creating URI.
+    Uri FilePathUri;
+
+    // Creating StorageReference and DatabaseReference object.
+    StorageReference disasterStorageReference;
+    DatabaseReference disasterDatabaseReference;
+
+    // Image request code for onActivityResult() .
+    int Image_Request_Code = 7;
+    // private ProgressDialog progressDialog;
+
+
+    //defining firebaseauth object
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        disasterStorageReference = FirebaseStorage.getInstance().getReference();
+        disasterDatabaseReference = FirebaseDatabase.getInstance().getReference(Database_Path);
+        chooseButtonDisaster = (Button)findViewById(R.id.ButtonChooseImage);
+        disasterImage = (ImageView)findViewById(R.id.ShowImageView);
+
+
+        chooseButtonDisaster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Creating intent.
+                Intent intent = new Intent();
+
+                // Setting intent type as image to select image from phone storage.
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Please Select Image"), Image_Request_Code);
+
+            }
+        });
 
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
@@ -41,11 +99,23 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         //initializing views
+        editDisasterName = (EditText) findViewById(R.id.disasterName);
+        editCriticality = (EditText) findViewById(R.id.criticality);
+        buutonUploadImage = (Button) findViewById(R.id.uploadDisasterImage);
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
-
         //displaying logged in user name
         textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        buutonUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Calling method to upload selected image on Firebase storage.
+                // UploadImageFileToFirebaseStorage();
+               // registerUser();
+
+            }
+        });
 
         //adding listener to button
         buttonLogout.setOnClickListener(this);
