dee21df6eaa50668958ea95665395cb4a9accc95
Swaroopg79 <s528802@mail.nwmissouri.edu>
2018-04-02 10:23:26 -0500

    Changes in profile activity page

--- app/src/main/java/com/example/s528802/studentregistartion/ProfileActivity.java
+++ app/src/main/java/com/example/s528802/studentregistartion/ProfileActivity.java
@@ -129,77 +129,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     // Creating UploadImageFileToFirebaseStorage method to upload image on storage.
-    public void UploadImageFileToFirebaseStorage() {
 
-        // Checking whether FilePathUri Is empty or not.
-        if (FilePathUri != null) {
-
-            // Setting progressDialog Title.
-            //  progressDialog.setTitle("Image is Uploading...");
-
-            // Showing progressDialog.
-            //   progressDialog.show();
-
-            // Creating second StorageReference.
-            StorageReference storageReference2nd = storageReference.child(Storage_Path + System.currentTimeMillis() + "." + GetFileExtension(FilePathUri));
-
-            // Adding addOnSuccessListener to second StorageReference.
-            storageReference2nd.putFile(FilePathUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                            // Getting image name from EditText and store into string variable.
-                            String TempFName = fname.getText().toString().trim();
-                            String TempLName = lname.getText().toString().trim();
-                            String TempEmail = editTextEmail.getText().toString().trim();
-                            String TempPassword = editTextPassword.getText().toString().trim();
-                            // Hiding the progressDialog after done uploading.
-                            // progressDialog.dismiss();
-
-                            // Showing toast message after done uploading.
-                            Toast.makeText(getApplicationContext(), "User Registered Successfully ", Toast.LENGTH_LONG).show();
-
-                            @SuppressWarnings("VisibleForTests")
-                            DataUploadInfo imageUploadInfo = new DataUploadInfo(TempFName,TempLName,TempEmail,TempPassword,taskSnapshot.getDownloadUrl().toString());
-                            //System.out.println("I am inside upload Image method");
-                            // Getting image upload ID.
-                            String ImageUploadId = databaseReference.push().getKey();
-
-                            // Adding image upload id s child element into databaseReference.
-                            databaseReference.child(ImageUploadId).setValue(imageUploadInfo);
-                        }
-                    })
-                    // If something goes wrong .
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-
-                            // Hiding the progressDialog.
-                            // progressDialog.dismiss();
-
-                            // Showing exception erro message.
-                            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    })
-
-                    // On progress change upload time.
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-
-                            // Setting progressDialog Title.
-                            // progressDialog.setTitle("Image is Uploading...");
-
-                        }
-                    });
-        }
-        else {
-
-            Toast.makeText(MainActivity.this, "Please Select Image or Add Image Name", Toast.LENGTH_LONG).show();
-
-        }
-    }
 
 
     @Override
